b7b24330cc6cf02a34535050ed982972f70632ab
diogenes-souza-dev <diogenes.cruz2001@gmail.com>
2022-05-04 09:45:01 -0300

    mudando ordem das classes para facilitar configuração de novos métodos

--- omie.py
+++ omie.py
@@ -3,6 +3,37 @@ import os
 import requests
 from dotenv import load_dotenv
 
+class Omie:
+    def __init__(self, empresa):
+        self.ListarProdutos = OmieListarProdutos(empresa)
+
+class OmieListarProdutos:
+
+    def __init__(self, empresa):
+        self.empresa = empresa
+        self.caminho = "geral/produtos/"
+        self.call = 'ListarProdutos'
+        self.pagina = 1
+        self.registros_por_pagina = 50
+        self.apenas_importado_api = 'N'
+        self.filtrar_apenas_omiepdv = 'N'
+
+    def executar(self):
+        return OmieApi().executar(self, self.empresa)
+
+    def todos(self):
+        self.registros_por_pagina = 100
+        consulta = self.executar()
+        total_de_paginas = consulta['total_de_paginas']
+        lista = consulta['produto_servico_cadastro']
+        while self.pagina < total_de_paginas:
+            self.pagina += 1
+            produtos = self.executar()['produto_servico_cadastro']
+            for produto in produtos:
+                lista.append(produto)
+
+        return lista
+
 class OmieApi:
     def __init__(self):
         self.caminho = ""
@@ -48,34 +79,3 @@ class OmieApi:
             novo[atributo] = valor     
 
         return novo
-
-class OmieListarProdutos:
-
-    def __init__(self, empresa):
-        self.empresa = empresa
-        self.caminho = "geral/produtos/"
-        self.call = 'ListarProdutos'
-        self.pagina = 1
-        self.registros_por_pagina = 50
-        self.apenas_importado_api = 'N'
-        self.filtrar_apenas_omiepdv = 'N'
-
-    def executar(self):
-        return OmieApi().executar(self, self.empresa)
-
-    def todos(self):
-        self.registros_por_pagina = 100
-        consulta = self.executar()
-        total_de_paginas = consulta['total_de_paginas']
-        lista = consulta['produto_servico_cadastro']
-        while self.pagina < total_de_paginas:
-            self.pagina += 1
-            produtos = self.executar()['produto_servico_cadastro']
-            for produto in produtos:
-                lista.append(produto)
-
-        return lista
-
-class Omie:
-    def __init__(self, empresa):
-        self.ListarProdutos = OmieListarProdutos(empresa)
